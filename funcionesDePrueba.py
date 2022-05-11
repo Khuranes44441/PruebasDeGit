12bf8c0088c2f6bc80dfa96458c5bf2166b25009
Khuranes44441 <perezernesto523@gmail.com>
2022-05-11 01:22:54 -0300

    funcion original de rama secundaria

---
+++ funcionesDePrueba.py
@@ -0,0 +1,8 @@
+def saludar(nombre):
+    print(f"hola mi nombre es {nombre}, te saludo desde la RAMA MASTER")
+
+def edad(edad):
+    print(f"tengo {edad} años")
+
+def funcion_creada_desde_rama_secundaria():
+    print("soy una funcion creada originalmente desde la rama secundaria")
