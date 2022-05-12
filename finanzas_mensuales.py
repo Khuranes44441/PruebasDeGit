bcb00c2111314b41a688e13125d1b2ba497a6789
Khuranes44441 <perezernesto523@gmail.com>
2022-05-12 19:27:02 -0300

    funcionalidad gastos mensuales agregada

--- finanzas_mensuales.py
+++ finanzas_mensuales.py
@@ -1,10 +1,8 @@
 
 
-def finanzasPersonales():
+def finanzasPersonales(mes):
     patrimonio_total = []
-    
-
-    # DINERO MODERACION
+    gastos_mensuales = []
 
     def sueldo_mensual(mes, cantidad_dias_3_horas_y_media, cantidad_dias_4_horas, cantidad_dias_4_horas_y_media, cantidad_dias_8_horas, progresar, ife):
 
@@ -26,11 +24,10 @@ def finanzasPersonales():
 
         paga_moderacion_mensual = paga0 + paga1 + paga2 + paga3
 
-        print(f"moderacion {mes}:  {paga_moderacion_mensual}")
         patrimonio_total.append(paga_moderacion_mensual)
 
 
-    # print(f"{patrimonio_total}")
+    
 
 # INGRESOS EXTRA
 
@@ -38,32 +35,51 @@ def finanzasPersonales():
         progresar = progresar
         ife = ife
         ingresos_extra_total = progresar + ife
-        print(f"ingresos extra {mes}: {ingresos_extra_total}")
+        # print(f"ingresos extra {mes}: {ingresos_extra_total}")
         patrimonio_total.append(ingresos_extra_total)
 
         print(f"SUELDO MENSUAL {mes}: {paga_moderacion_mensual + ingresos_extra_total}")
 
-# EJECUCION DEL CODIGO
 
-    
+# MESES TRABAJADOS
 
     sueldo_mensual("mayo",cantidad_dias_3_horas_y_media=1, cantidad_dias_4_horas=6, cantidad_dias_4_horas_y_media=5, cantidad_dias_8_horas=0,progresar=12840, ife= 9000 )
     
-
     sueldo_mensual("junio",0, 10, 0, 0, 6400,9000)
 
-    sueldo_mensual("julio",0,10,0,0,6400,0)
-    
+    # sueldo_mensual("julio",0,10,0,0,6400,0)
 
-        
+    # sueldo_mensual("agosto",0,10,0,0,6400,0)
+
+    # sueldo_mensual("septiembre",0,10,0,0,6400,0)
+
+    # sueldo_mensual("octubre",0,10,0,0,6400,0)
+
+    # sueldo_mensual("noviembre",0,10,0,0,6400,0)
+
+    # sueldo_mensual("diciembre",0,10,0,0,6400,0)
+
+
+    def gastos(mes):
+        comida = 5000
+        vicios = 3000
+        gastos_totales_mensuales = comida + vicios
+        gastos_mensuales.append(gastos_totales_mensuales)
+        print(f"gastos extra acumulados al mes de {mes}: {sum(gastos_mensuales)} pesos")
+
+    gastos("junio")
+    # gastos("julio")
+    # gastos("agosto")
+
 
     def calculo_patrimonio_total ():
-        
-        print(f"el patrimonio neto total para el mes de julio es de {sum(patrimonio_total)}")
+            
+            print(f"patrimonio neto total {mes}: {sum(patrimonio_total)}")
+            print(f"patrimonio despues del gastos extra del mes de {mes}: {sum(patrimonio_total) - sum(gastos_mensuales)}")
 
     calculo_patrimonio_total()
 
 
+finanzasPersonales("junio")
 
 
-finanzasPersonales()
